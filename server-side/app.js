33825837844b042b82d56c009ab8b43fc1d2f80a
David J Crockett <crockett.davidj@gmail.com>
2022-12-13 23:42:16 -0800

    Connected mongoose

--- server-side/app.js
+++ server-side/app.js
@@ -1,6 +1,25 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
