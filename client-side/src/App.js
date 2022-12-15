298eda495950e1a046708c768df010e92b60bc29
David J Crockett <crockett.davidj@gmail.com>
2022-12-14 16:49:56 -0800

    Cleaning up architecture before coding

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello, World!</h1>
-      </header>
-    </div>
+    <Router> 
+            <Navbar /> 
+            <br/> 
+                <Routes>
+                  // home path
+                  // edit path
+                  // create path
+                  // user path
+                </Routes>
+        </Router>
   );
 }
 
